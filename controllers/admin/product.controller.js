514a766e71437d5b7aa05d72985bde8c829ed949
Kietnormal <kietleoffical75@gamil.com>
2025-04-13 15:11:41 +0700

    uploadv3

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,46 +1,25 @@
 const Product = require("../../model/product.model");
+const filerStatushelper = require("../../helpers/filerStatus");
+const searchHelper=require("../../helpers/search")
 module.exports.product = async (req, res) => {
-  let filerStatus = [
-    {
-      name: "Tất cả",
-      status: "",
-      class: "",
-    },
-    {
-      name: "Hoạt động",
-      status: "active",
-      class: "",
-    },
-    {
-      name: "Dừng hoạt động",
-      status: "inactive",
-      class: "",
-    },
-  ];
+  //bộ lọc
+
+  const filerStatus = filerStatushelper(req.query);
   const find = {
     deleted: "false",
   };
   if (req.query.status) {
-    const index=filerStatus.findIndex(item=>item.status==req.query.status)
-    filerStatus[index].class='active'
-   
-   
-  } else {
-   
-     const index=filerStatus.findIndex(item=>item.status=="")
-    filerStatus[index].class='active'
-   
-  }
-  if (req.query.status) {
     find.status = req.query.status;
   }
-  let keyword='';
-  if (req.query.keyword){
-    keyword=req.query.keyword;
-    find.title = keyword;
-  }
+ //tìm kiếm
+const objectSearch=searchHelper(req.query)
+if(objectSearch.regex){
+  find.title=objectSearch.regex
+}
+ 
 
   const product = await Product.find(find);
+ 
   product.forEach((item) => {
     item.pricenew = (
       (item.price * (100 - item.discountPercentage)) /
@@ -53,7 +32,7 @@ module.exports.product = async (req, res) => {
     pagetitle: "Trang Product",
     products: product,
     filerStatus: filerStatus,
-    keyword:keyword
+    keyword: objectSearch.keyword,
   });
   // res.send("trang sản phẩm")
   // res.send('trang admin')
